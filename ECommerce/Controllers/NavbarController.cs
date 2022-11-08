a78a082727b39f6bc6b3b6be0adaf39412ce6c05
Sadat Abdulla Al Muniem <sadatjnu@gmail.com>
2022-11-08 12:50:40 +0600

    adminlte implement

--- ECommerce/Controllers/NavbarController.cs
+++ ECommerce/Controllers/NavbarController.cs
@@ -1,6 +1,10 @@
-﻿using ECommerce.BusinessLogics.Helpers;
+﻿using ECommerce.BusinessLogics;
+using ECommerce.BusinessLogics.AppData;
+using ECommerce.BusinessLogics.Helpers;
+using ECommerce.BusinessLogics.ModelView;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,6 +27,22 @@ namespace ECommerce.Controllers
 
                 var data = new Data();
                 return PartialView("_LeftNavbar", data.NavbarItems(userRole).ToList());
+            }
+            catch (Exception ex)
+            {
+                return PartialView(ex.Message);
+            }
+        }
+
+        public ActionResult RightSideBar()
+        {
+            try
+            {
+                List<NavBarViewModel> lst = new List<NavBarViewModel>();
+                string userRole = "2";
+                DataTable dt = DataAccess.Instance.sellService.GetBySpWithParam("SP_GET_NAVBAR_LIST", new object[] { userRole });
+                lst = APIUitility.ConvertDataTable<NavBarViewModel>(dt);
+                return PartialView("_LeftNavbar", lst);
             }
             catch (Exception ex)
             {
